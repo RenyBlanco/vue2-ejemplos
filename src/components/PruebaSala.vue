7d25fc666e1a77b96840c31ccc1d041bc3776b74
RenyBlanco <rey.blanco@yahoo.com>
2023-04-10 13:53:50 -0400

    Elimino node_modules

---
+++ src/components/PruebaSala.vue
@@ -0,0 +1,26 @@
+<template>
+    <h2>Listados...</h2>
+    <ul>
+        <li>Nombre: {{this.empleado.name}}</li>
+        <li>Edad: {{this.empleado.age}}</li>
+        <li>Cargo: {{this.empleado.designation}}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+  name: "PruebaSala",
+  data: function() {
+    return {
+      empleado: {
+        name: "Sala",
+        age: 25,
+        designation: "Software Engineer"
+      }
+    };
+  } 
+};
+</script>
+
+<style>
+</style>
